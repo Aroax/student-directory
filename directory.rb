4502b63359b4dc16d4d2d6bc1aa7e80845a92933
Arran Oakes <arran.oakes@gmail.com>
2021-05-26 09:07:35 +0100

    refactored by adding methods

--- directory.rb
+++ directory.rb
@@ -15,9 +15,21 @@ students = [
 ]
 
 # and then print them
-puts "The students of Villains Academy"
-puts "--------------"
-students.each {|student| puts student}
+def print_header
+  puts "The students of Villains Academy"
+  puts "--------------"
+end
+
+def print(names)
+names.each {|name| puts name}
+end
 
 # finally, we pritn the total number of students
-puts "Overall we have #{students.count} great students"
+def print_footer(names)
+  puts "Overall we have #{names.count} great students"
+end
+
+# nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
