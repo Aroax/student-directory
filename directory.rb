e879fc1f4c4edd227f5f6e874f62b2c355f47eae
Arran Oakes <arran.oakes@gmail.com>
2021-05-28 08:48:27 +0100

    fixed load bug

--- directory.rb
+++ directory.rb
@@ -62,7 +62,7 @@ end
 
 # Load student list from CSV file
 def load_students
-  file = File.open ("students.csv", "r")
+  file = File.open("students.csv", "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
